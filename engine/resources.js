76d2c4f6f30fd16915f84f2adae936770004b21d
MikezuGe <MikezuGe@users.noreply.github.com>
2017-10-04 19:07:45 +0300

    Added texture loading from images

--- engine/resources.js
+++ engine/resources.js
@@ -175,14 +175,10 @@
 
         static loadOverride(sourceUrl, callback) {
             // TODO
-            console.log("asd");
             let image = new Image();
-            //image.crossOrigin = "anonymous";
             image.onload = function() {
-                console.log("asde");
                 callback(image);
             }
-            console.log(sourceUrl);
             image.src = sourceUrl;
         }
         
@@ -198,16 +194,21 @@
         
         
         static makeTexture(format, width, height, color) {
-            // TODO
             /// Creates a texture manually. In most cases used if no texture is available
             /// Format: RGBA / RGBA
             /// Width and height: texture dimensions
             /// Color: Color.white.toArray()
+            
+            let gl = engine.gl;
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.texImage2D(gl.TEXTURE_2D, 0, format, width, height, 0, format, gl.UNSIGNED_BYTE, new Uint8Array([...color]));
+            // Unbind texture so no accidents happen
+            gl.bindTexture(gl.TEXTURE_2D, null);
+            return texture;
         }
         
         
         static _uploadToGPU(img) {
-            console.log(img);
             // TODO: And here's where the magic happens.
             let gl = engine.gl;
             
@@ -215,7 +216,7 @@
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
             
-            /// If power of 2 width and height, generate mipmap
+            // If power of 2 width and height, generate mipmap
             if(img.width && img.height && !(img.width & (img.width - 1)) && !(img.height & (img.height - 1)))
                 gl.generateMipmap(gl.TEXTURE_2D);
             else {
@@ -223,7 +224,7 @@
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
                 gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             }
-            /// Unbind texture so no accidents happen
+            // Unbind texture so no accidents happen
             gl.bindTexture(gl.TEXTURE_2D, null);
             
             return texture;
