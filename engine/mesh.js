44f11b9897861a3f93be35e86659403355629ab8
MikezuGe <MikezuGe@users.noreply.github.com>
2017-10-07 17:14:59 +0300

    Add files via upload

--- engine/mesh.js
+++ engine/mesh.js
@@ -1,10 +1,10 @@
-/* global engine, Geometry, Resource, buildTestMesh */
+/* global engine, Geometry, Resource */
 "use strict";
 
 (function(context) {
 
     context.MeshAttributeNames = [
-        "position", "color", "texCoord", "normal", "tangent"
+        "position", "color", "texCoord", "normal", "tangent", "bitangent"
     ];
 
     /**
@@ -77,9 +77,8 @@
                     new Geometry(0, indices.length, null)
                 ];
             }
-
+            console.log(vertices);
             let gl = engine.gl;
-
             // Upload the vertices to the gpu
             let vb = gl.createBuffer();
             gl.bindBuffer(gl.ARRAY_BUFFER, vb);
@@ -149,13 +148,13 @@
             let textCoords = data.slice(data.indexOf('vt '), data.indexOf('vn '));
             let normals    = data.slice(data.indexOf('vn '), data.indexOf('f '));
             let indices    = data.slice(data.indexOf('f '),  data.length);
-            
+
             vertices   = vertices.match(/-?\d\.\d*/g).map(parseFloat);
             textCoords = textCoords.match(/\d\.\d*/g).map(parseFloat);
             normals    = normals.match(/-?\d\.\d*/g).map(parseFloat);
             indices    = indices.match(/\d+/g).map(function(num) { return parseInt(num, 10) - 1; });
-            
-            
+
+
             let v = [], t = [], n = [], ind = [];
             let ilen = indices.length;
             let count = 0;
@@ -166,8 +165,8 @@
                     vertices[indices[count] * 3 + 1],
                     vertices[indices[count] * 3 + 2]);
                 count++;
-                t.push(textCoords[indices[count] * 3 + 0],
-                    textCoords[indices[count] * 3 + 1]);
+                t.push(textCoords[indices[count] * 2 + 0],
+                    textCoords[indices[count] * 2 + 1]);
                 count++;
                 n.push(normals[indices[count] * 3 + 0],
                     normals[indices[count] * 3 + 1],
@@ -175,7 +174,41 @@
                 count++;
             }
 
-            return Mesh.buildMesh(v, t, n, ind);
+            let tan = [], bitan = [];
+            let vAmount = v.length;
+            for(let i = 0; i < vAmount; i+=9) {
+
+                let v1 = [ v[i + 0], v[i + 1], v[i + 2] ];
+                let v2 = [ v[i + 3], v[i + 4], v[i + 5] ];
+                let v3 = [ v[i + 6], v[i + 7], v[i + 8] ];
+                
+				let uvi = i / 3 * 2;
+				let uv1 = [ t[uvi + 0], t[uvi + 1] ];
+				let uv2 = [ t[uvi + 2], t[uvi + 3] ];
+				let uv3 = [ t[uvi + 4], t[uvi + 5] ];
+				
+				let x1 = v2[0] - v1[0];
+				let x2 = v3[0] - v1[0];
+				let y1 = v2[1] - v1[1];
+				let y2 = v3[1] - v1[1];
+				let z1 = v2[2] - v1[2];
+				let z2 = v3[2] - v1[2];
+
+				let s1 = uv2[0] - uv1[0];
+				let s2 = uv3[0] - uv1[0];
+				let t1 = uv2[1] - uv1[1];
+				let t2 = uv3[1] - uv1[1];
+
+				let r = 1.0 / (s1 * t2 - s2 * t1);
+				let sdir = vec3.normalize([(t2 * x1 - t1 * x2) * r, (t2 * y1 - t1 * y2) * r, (t2 * z1 - t1 * z2) * r]);
+				let tdir = vec3.normalize([(s1 * x2 - s2 * x1) * r, (s1 * y2 - s2 * y1) * r, (s1 * z2 - s2 * z1) * r]);
+                
+				tan.push(...sdir, ...sdir, ...sdir);
+				bitan.push(...tdir, ...tdir, ...tdir);
+				
+            }
+			
+            return Mesh.buildMesh(v, t, n, ind, tan, bitan);
         }
 
         /**
@@ -190,7 +223,7 @@
          *
          * Oh lord forgive me for I have sinned...
          */ 
-        static buildMesh(positions, uvs, normals, indices) {
+        static buildMesh(positions, uvs, normals, indices, tangents, bitangents) {
 
             if ((positions.length % 3) != 0) {
                 console.log("Vertex attribute lengths not multiply of 3");
@@ -198,7 +231,7 @@
             }
 
             let vCount = positions.length / 3;
-            let vSize = 8; // sizeof(position) + sizeof(texcoord) + sizeof(normal)
+            let vSize = 14; // sizeof(position) + sizeof(texcoord) + sizeof(normal) + sizeOf(tangent) + sizeOf(bitangent)
 
             let vBuff = new Float32Array(vCount * vSize);
 
@@ -215,6 +248,14 @@
                 vBuff[offset + 5] = normals[i * 3 + 0];
                 vBuff[offset + 6] = normals[i * 3 + 1];
                 vBuff[offset + 7] = normals[i * 3 + 2];
+
+                vBuff[offset + 8] = tangents[i * 3 + 0];
+                vBuff[offset + 9] = tangents[i * 3 + 1];
+                vBuff[offset + 10] = tangents[i * 3 + 2];
+
+                vBuff[offset + 11] = bitangents[i * 3 + 0];
+                vBuff[offset + 12] = bitangents[i * 3 + 1];
+                vBuff[offset + 13] = bitangents[i * 3 + 2];
             }
 
             let iBuff = new Uint16Array(indices);
@@ -223,10 +264,12 @@
                 new context.MeshAttribute("position", 0, 3),
                 new context.MeshAttribute("texCoord", 3, 2),
                 new context.MeshAttribute("normal", 5, 3),
+                new context.MeshAttribute("tangent", 8, 3),
+                new context.MeshAttribute("bitangent", 11, 3)
             ];
-
+			
             return Mesh.fromData(vBuff, iBuff, attrs);
-        }
+		}
 
     };
 })(this);
